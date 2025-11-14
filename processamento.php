b806585f795491dca7708925e3f7845a01f73b0b
JPMSz <jpmds16@gmail.com>
2025-11-13 21:04:16 -0300

    mudanças no processamento 5

--- processamento.php
+++ processamento.php
@@ -5,20 +5,20 @@ session_start();
 require "funcoesBD.php";
 
 //Cadastro de Cliente
-if(!empty($_POST['inputnome']) && !empty($_POST['inputsobrenome']) && !empty($_POST['inputcpf']) && !empty($_POST['inputdataNascimento']) && !empty($_POST['inputtelefone']) && !empty($_POST['inputemail']) && !empty($_POST['inputsenha'])){
+if(!empty($_POST['nome']) && !empty($_POST['sobrenome']) && !empty($_POST['cpf']) && !empty($_POST['dataNascimento']) && !empty($_POST['telefone']) && !empty($_POST['email']) && !empty($_POST['senha'])){
 
 
-$nome = $_POST['inputnome'];
-$sobrenome = $_POST['inputsobrenome'];
-$cpf = $_POST['inputcpf'];
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$cpf = $_POST['cpf'];
 $dataNascimento = $_POST['dataNascimento'];
-$telefone = $_POST['inputtelefone'];
-$email = $_POST['inputemail'];
-$senha = $_POST['inputsenha'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
 inserirCliente($nome, $sobrenome, $cpf, $dataNascimento, $telefone, $email, $senha);
 
-echo "Nome: ". $nome. "Sobrenome: ". $sobrenome. "CPF: ". $cpf. "Data de nascimento: ". $dataNascimento. "Telefone: ". $telefone. "Email: ". $email. "Senha: ". $senha; 
+//echo "Nome: ". $nome. "Sobrenome: ". $sobrenome. "CPF: ". $cpf. "Data de nascimento: ". $dataNascimento. "Telefone: ". $telefone. "Email: ". $email. "Senha: ". $senha; 
 
 header('location:CadastrarCliente.php');
 die();
